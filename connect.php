c83e4c477a7b028531d3077f1d01193adb240636
Wolterix <91145581+Wolterix@users.noreply.github.com>
2021-09-22 23:46:05 +0200

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,8 +1,8 @@
 <?php
 
-	$host = "localhost";
-	$db_user = "root";
-	$db_password = "";
-	$db_name = "osadnicy";
+  $host = "mysql53.mydevil.net";
+  $db_user = "m1409";
+  $db_password = "9UdZ2A%AGKZv2^QBcwJi";
+  $db_name = "m1409_wolterlog";
 
 ?>
